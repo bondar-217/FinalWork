c58e4885c16cdcaac72238db75e99a08357cf2b2
bondar-217 <126788617+bondar-217@users.noreply.github.com>
2023-05-09 21:24:23 +0300

    загрузил программу

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+
+Console.Write("Input data separated by ', ':");
+string? str = Console.ReadLine();
+string[] array = str.Split(", ")
+                     .ToArray();
+
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+string[] FindShortString(string[] array)
+{
+    int index = 0;
+    string[] newArray = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            index++;
+            newArray[index] = array[i];
+        }
+    }
+    return newArray;
+}
+
+ShowArray(array);
+
+string[] myArray = FindShortString(array);
+ShowArray(myArray);
